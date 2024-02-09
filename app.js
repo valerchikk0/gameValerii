7bebd245a817c236e7389d807e740eeb12c396aa
Valerii <erbaev.valera@gmail.com>
2024-02-09 14:02:12 +0100

    adding code

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("startBtn").addEventListener("click", function() {
+        window.location.href = "page1.html";
+    });
+
+    document.getElementById("continueBtn").addEventListener("click", function() {
+        window.location.href = "page3.html";
+    });
+
+    document.getElementById("getUpBtn").addEventListener("click", function() {
+        window.location.href = "page2.html";
+    });
+
+    document.getElementById("visitCasinoBtn").addEventListener("click", function() {
+        window.location.href = "page5.html";
+    });
+
+    document.getElementById("investWiselyBtn").addEventListener("click", function() {
+        window.location.href = "page4.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("winFightBtn").addEventListener("click", function() {
+        window.location.href = "page7.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("quitGameBtn").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
